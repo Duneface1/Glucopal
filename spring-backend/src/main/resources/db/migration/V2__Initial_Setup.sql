fef6a4894c3e76ab25bf447c5995e0f1c5730591
Samuelt5gaming <samuelwamukui12@gmail.com>
2026-04-24 17:11:32 +0300

    1

--- spring-backend/src/main/resources/db/migration/V2__Initial_Setup.sql
+++ spring-backend/src/main/resources/db/migration/V2__Initial_Setup.sql
@@ -1,7 +1,7 @@
 
-DROP VIEW IF EXISTS v_today_glucose_records CASCADE;
+
 DROP VIEW IF EXISTS v_active_glucose_records CASCADE;
-DROP VIEW IF EXISTS your_glucose_view_name CASCADE;
+
 
 ALTER TABLE glucose_records ALTER COLUMN id SET DATA TYPE bigint;
 
@@ -10,7 +10,3 @@ CREATE VIEW v_active_glucose_records AS
     FROM glucose_records
     WHERE is_active = true;
 
-CREATE VIEW v_today_glucose_records AS
-    SELECT *
-    FROM v_active_glucose_records
-    WHERE DATE(recorded_at) = CURRENT_DATE;
